28ff65c6b913d8a07cb41554e0989caceac8c05f
Manoranjan Kumar <baskeymanoranjan400@gmail.com>
2022-09-04 09:44:03 +0530

    new codes added

--- file.cpp
+++ file.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+     int a;
+    cout<<"enter the no. of times";
+    cin>>a;
+    for(i=1;i<a;i++)
+    {
+        cout<<"hello world"<<endl;
+    }
+    }
+}
